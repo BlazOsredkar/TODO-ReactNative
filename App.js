55148f269b546bbaedf9a0a27de12cb3ab8ee88f
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-12-08 12:37:54 +0100

    Revert "."
    This reverts commit d279f185cd8b763f8484ebb84ddb6fe387cdf2e7.

--- App.js
+++ App.js
@@ -1,44 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View, FlatList, Button, AsyncStorage } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { useState } from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
-
-  useEffect(() => {
-    // Load stored data on app start
-    loadGoals();
-  }, []);
-
-  useEffect(() => {
-    // Save data whenever courseGoals state changes
-    saveGoals();
-  }, [courseGoals]);
-
-  // Function to load goals from AsyncStorage
-  const loadGoals = async () => {
-    try {
-      const storedGoals = await AsyncStorage.getItem("courseGoals");
-      if (storedGoals !== null) {
-        setCourseGoals(JSON.parse(storedGoals));
-      }
-    } catch (error) {
-      console.error("Error loading goals:", error);
-    }
-  };
-
-  // Function to save goals to AsyncStorage
-  const saveGoals = async () => {
-    try {
-      await AsyncStorage.setItem("courseGoals", JSON.stringify(courseGoals));
-    } catch (error) {
-      console.error("Error saving goals:", error);
-    }
-  };
 
   function startAddGoalHandler() {
     setModalIsVisible(true);
@@ -102,19 +71,10 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     paddingTop: 50,
     paddingHorizontal: 16,
-    backgroundColor: "#fff",
   },
   goalsContainer: {
-    flex: 1,
-    justifyContent: "center",
-    width: "100%",
-    paddingHorizontal: 16,
-  },
-  addGoalButton: {
-    marginBottom: 20,
+    flex: 5,
   },
 });
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,32 +1,11 @@
-import React from "react";
-import { StyleSheet, View, Text, Pressable } from "react-native";
-import { AsyncStorage } from "react-native";
-
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem(props) {
-  const deleteGoalHandler = async (id) => {
-    await deleteGoalFromStorage(id); // Delete the goal from AsyncStorage
-    props.onDeleteItem(id);
-  };
-
-  async function deleteGoalFromStorage(id) {
-    try {
-      const storedGoals = await AsyncStorage.getItem("courseGoals");
-      if (storedGoals !== null) {
-        const parsedGoals = JSON.parse(storedGoals);
-        const updatedGoals = parsedGoals.filter((goal) => goal.id !== id);
-        await AsyncStorage.setItem("courseGoals", JSON.stringify(updatedGoals));
-      }
-    } catch (error) {
-      console.error("Error deleting goal:", error);
-    }
-  }
-
   return (
     <View style={styles.goalItem}>
       <Pressable
-        android_ripple={{ color: "#210644" }}
-        onPress={() => deleteGoalHandler(props.id)} // Use an arrow function here
+        android_ripple={{ color: '#210644' }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
         style={({ pressed }) => pressed && styles.pressedItem}
       >
         <Text style={styles.goalText}>{props.text}</Text>
@@ -41,13 +20,13 @@ const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
     borderRadius: 6,
-    backgroundColor: "#5e0acc",
+    backgroundColor: '#5e0acc',
   },
   pressedItem: {
     opacity: 0.5,
   },
   goalText: {
-    color: "white",
+    color: 'white',
     padding: 8,
   },
 });
